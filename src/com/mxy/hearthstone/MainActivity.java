3bdc283b4b35632460c0b507007f360260b76b01
SinTi <jaymengsniper@gmail.com>
2014-09-05 23:06:40 +0800

    消耗筛选按钮样式

--- src/com/mxy/hearthstone/MainActivity.java
+++ src/com/mxy/hearthstone/MainActivity.java
@@ -19,37 +19,41 @@ public class MainActivity extends Activity {
     private ImageView iv;
     private AlphaAnimation aa;
     private ScaleAnimation sa;
-    public SQLiteDatabase cardDb;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        iv = (ImageView) findViewById(R.id.iv_startImage);
-        sa = new ScaleAnimation(1, 2, 1, 2, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
-
-        sa.setDuration(2000);
-        sa.setFillAfter(true);
-        iv.startAnimation(sa);
-
-        sa.setAnimationListener(new Animation.AnimationListener() {
-            @Override
-            public void onAnimationStart(Animation animation) {
-            }
-
-            @Override
-            public void onAnimationEnd(Animation animation) {
-                aa = new AlphaAnimation(1, 0);
-                aa.setFillAfter(true);
-                iv.startAnimation(aa);
-            }
-
-            @Override
-            public void onAnimationRepeat(Animation animation) {
-
-            }
-        });
         copyDB();
+        new Thread(){
+            @Override
+            public void run() {
+                iv = (ImageView) findViewById(R.id.iv_startImage);
+                sa = new ScaleAnimation(1, 2, 1, 2, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+
+                sa.setDuration(2000);
+                sa.setFillAfter(true);
+                iv.startAnimation(sa);
+
+                sa.setAnimationListener(new Animation.AnimationListener() {
+                    @Override
+                    public void onAnimationStart(Animation animation) {
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+                        aa = new AlphaAnimation(1, 0);
+                        aa.setFillAfter(true);
+                        iv.startAnimation(aa);
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {
+
+                    }
+                });
+            }
+        }.start();
     }
 
     private void copyDB() {
@@ -57,10 +61,11 @@ public class MainActivity extends Activity {
             new Thread() {
                 public void run() {
                     try {
-                        /*File zipFile = new File("/db.zip");
-                        ZipUtil.upZipFile(zipFile,MainActivity.this.getApplicationInfo().dataDir + "/db/");
-                        File imgFile = new File("/images.zip");
-                        ZipUtil.upZipFile(imgFile,MainActivity.this.getApplicationInfo().dataDir + "/db/images/");*/
+//                        File zipFile = new File("/db.zip");
+//                        ZipUtil.upZipFile(zipFile,MainActivity.this.getApplicationInfo().dataDir + "/db/");
+//                        File imgFile = new File("/images.zip");
+//                        ZipUtil.upZipFile(imgFile,MainActivity.this.getApplicationInfo().dataDir + "/db/images/");
+
                         ZipInputStream dbZipInputStream = new ZipInputStream(MainActivity.this.getAssets().open("db.zip"));
                         ZipUtil.ectract(dbZipInputStream, MainActivity.this.getApplicationInfo().dataDir + "/db/");
                         ZipInputStream imgZipInputStream = new ZipInputStream(MainActivity.this.getAssets().open("images.zip"));
